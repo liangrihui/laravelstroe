d0c85ebcecac7efc0d17c94d21641e2083090b58
liangrihui <1239181489@qq.com>
2018-07-25 08:06:12 +0800

    change model
    定义模型

--- other/src/Models/User.php
+++ other/src/Models/User.php
@@ -3,6 +3,7 @@ namespace LRH\Models;
 
 class User extends \Illuminate\Database\Eloquent\Model
 {
+    protected $fillable = ['id'];
     public function roles()
     {
         return $this->belongsToMany(Role::class);
